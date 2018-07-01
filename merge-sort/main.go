1d908b43c8ab89528b456d8fe96e0f13723052c3
Max Prosper <maxim.yaaman@gmail.com>
2018-07-01 21:31:56 +0300

    Annotations added to MergeSort

--- merge-sort/main.go
+++ merge-sort/main.go
@@ -12,32 +12,33 @@ func main() {
 	fmt.Printf("SORTED: %v \n", nums)
 }
 
+// this helper function creates two slices based on the length and values of the first and second halves of an original slice
+// then values of these two slice compared one by one
+// on each iteration, smaller value copied to the original slice
 func merge(nums *[]int, s, m, e int) *[]int {
 
-	n1 := m - s + 1
-	n2 := e - m
+	n1 := m - s + 1 // calculate the length of the left slice and right slices
+	n2 := e - m     // 's' - start, 'm' - middle, 'e' - end
 
-	left := make([]int, n1)
-	right := make([]int, n2)
+	left, right := make([]int, n1), make([]int, n2) // make two slices of length n1 and n2
 
 	for i := 0; i < n1; i++ {
-		left[i] = (*nums)[s+i]
+		left[i] = (*nums)[s+i] // fill first slice with values of the first half of 'nums' slice
 	}
 
 	for j := 0; j < n2; j++ {
-		right[j] = (*nums)[m+j+1]
+		right[j] = (*nums)[m+j+1] // fill second slice with values of the second half of 'nums' slice
 	}
 
-	left = append(left, math.MaxUint32)
-	right = append(right, math.MaxUint32)
+	left = append(left, math.MaxUint32)   // add a 'signal' value (max possible uint32) to the end of both slices
+	right = append(right, math.MaxUint32) // any integer can't be bigger than this value
 
-	i := 0
-	j := 0
+	i, j := 0, 0 // initialize variable to work with indecies
 
 	for k := s; k <= e; k++ {
-		if left[i] <= right[j] {
-			(*nums)[k] = left[i]
-			i++
+		if left[i] <= right[j] { // compare first values of left and rigth slices
+			(*nums)[k] = left[i] // and write a smaller one to 'sum' slice at the index of iteration
+			i++                  // increase the index for the slice smaller value was copied from
 		} else {
 			(*nums)[k] = right[j]
 			j++
@@ -46,12 +47,13 @@ func merge(nums *[]int, s, m, e int) *[]int {
 	return nums
 }
 
+// this function uses the helper 'merge' function in a recursion calls
 func mergeSort(nums *[]int, s, e int) *[]int {
-	if s < e {
-		m := int((s + e) / 2)
-		mergeSort(nums, s, m)
-		mergeSort(nums, m+1, e)
-		merge(nums, s, m, e)
+	if s < e { // if 's' is no smaller than 'e', left and right slices lenghts are 1 and they are sorted
+		m := int((s + e) / 2)   // define the 'meiddle' point by which the original slice will be divided
+		mergeSort(nums, s, m)   // call 'mergeSort' on the first half recursively
+		mergeSort(nums, m+1, e) // call 'mergeSort' on the second half recursively
+		merge(nums, s, m, e)    // use 'merge' to sort halved slice and write new soreted values to 'sum' on each recursive call and
 
 		return nums
 	}
